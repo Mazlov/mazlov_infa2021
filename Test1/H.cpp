cf9326291070cb607ae5382cced492e5dd3fb637
Mazlov <mazlov.ip@phystech.edu>
2021-03-02 00:27:10 +0300

    Added H.

--- Test1/H.cpp
+++ Test1/H.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 
-int main()
-{
-    int a, b;
-    std::cin >> a;
-    b = a;
-    for (int i = 2;  i  <= b;  i++) {
-        while (a % i == 0) {
-            std::cout << i << std::endl;
-            a = a / i;
+int main() {
+    int S;
+    std::string N;
+    std::cin >> S >> N;
+    int count = 0;
+    while (N != "A999AA") {
+        if (S  > 60) {
+            char f = N[1];
+            char s = N[2];
+            char t = N[3];
+            if (f == s && s == t){
+                count += 1000;
+            }
+            else if ((f == s && s != t) || (f == t && s != t) || (s == t && f != t)){
+                count += 500;
+            }
+            if (f != s && s != t && f != t){
+                count += 100;
+            }
         }
+        std::cin >> S >> N;
     }
+    std::cout << count;
     return 0;
 }
